fae758a219ba9111c44a295bad7934b15ad42dcc
Chaitanya Varu <98262685+hokageCV@users.noreply.github.com>
2022-07-21 18:21:55 +0530

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/soc_macmini_15/musicplayer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/soc_macmini_15/musicplayer/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.soc_macmini_15.musicplayer;
+package  com.example.soc_macmini_15.musicplayer;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
